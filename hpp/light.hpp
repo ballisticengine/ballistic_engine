917d3622246ab41f54497cb10c22db9391cc0e08
Maciek <maciej@webtotal.no>
2014-06-23 20:57:33 +0000

    uml lights

---
+++ hpp/light.hpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   light.hpp
+ * Author: maciek
+ *
+ * Created on June 23, 2014, 8:53 PM
+ */
+
+#ifndef LIGHT_HPP
+#define	LIGHT_HPP
+
+#include "entity.hpp"
+class light : public entity {
+    
+};
+
+#endif	/* LIGHT_HPP */
+
