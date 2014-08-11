7d4cb5029293e21b8b5df65adef44a35cd8cf2ea
unknown <maciej@z.xx>
2014-08-11 23:16:34 +0200

    simple ctrl

--- cpp/sdlControls.cpp
+++ cpp/sdlControls.cpp
@@ -1,11 +1,31 @@
 #include "sdlControls.hpp"
 
 void sdlControls::operator()() {
+	camera *c=world::getInstance()->getCurrentCamera();
+	e_loc step=10;
+	int x,y;
+
 	while(!engineState::getInstance()->exit()) {
 	 const Uint8 *state = SDL_GetKeyboardState(NULL);
+	 SDL_GetRelativeMouseState(&x,&y);
 	 if(state[SDL_SCANCODE_RIGHT]) {
-	  cout << "RIGHT";
+		 c->translate(-step,0,0);
+	 }
+	 
+	 if(state[SDL_SCANCODE_LEFT]) {
+	 	 c->translate(step,0,0);
 	 }
 
+	 if(state[SDL_SCANCODE_UP]) {
+	 	 c->translate(0,0,step);
+	 }
+
+	 if(state[SDL_SCANCODE_DOWN]) {
+	 	 c->translate(0,0,-step);
+	 }
+	 c->rotate(0,x,0);
+	 cout << x << endl;
+	SDL_Delay(100);
 	}
+	
 }
