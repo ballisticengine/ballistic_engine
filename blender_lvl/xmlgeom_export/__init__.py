c1daa7a83c765b2309078164a3c5b7f51c13c444
unknown <maciej@z.xx>
2014-08-14 13:40:25 +0200

    exporter into plugin

--- blender_lvl/xmlgeom_export/__init__.py
+++ blender_lvl/xmlgeom_export/__init__.py
@@ -16,7 +16,50 @@ from bpy_extras.io_utils import (ImportHelper,
                                  axis_conversion,
                                  )
 
-import export_helper as exp
+import xml.etree.ElementTree as ET
+
+
+
+#HELPER START
+
+# bpy.ops.object.mode_set(mode='OBJECT')
+
+def determineTexture(model):
+    img=''
+    for t in model.data.uv_textures.active.data:
+     if t.image:
+        img = t.image.name
+    return img
+
+def shapeExport(model,scale=1):
+        #scene = context.scene
+        shape=ET.Element('shape')
+        texture=ET.SubElement(shape,'texture')
+        name=ET.SubElement(shape,'name')
+        geom=ET.SubElement(shape,'geom')
+        name.text=model.data.name
+        texture.text=determineTexture(model)
+        for f in model.data.polygons:
+            face=ET.SubElement(geom,'face')
+            for idx in f.vertices:
+                vertex=ET.SubElement(face,'vertex')
+                x=ET.SubElement(vertex,'x')
+                y=ET.SubElement(vertex,'y')
+                z=ET.SubElement(vertex,'z')
+                u=ET.SubElement(vertex,'u')
+                v=ET.SubElement(vertex,'v')
+                x.text=str(model.data.vertices[idx].co[0]*scale)
+                y.text=str(model.data.vertices[idx].co[1]*scale)
+                z.text=str(model.data.vertices[idx].co[2]*scale)
+            for j,ul in enumerate(model.data.uv_layers):
+                u.text=str(ul.data[idx].uv[0])
+                v.text=str(ul.data[idx].uv[1])
+        return shape
+
+#HELPER END
+
+
+
 
 
 class ExportXML(bpy.types.Operator,ExportHelper):
@@ -31,7 +74,14 @@ class ExportXML(bpy.types.Operator,ExportHelper):
             )
 
     def execute(self, context):
-        exp.shapeExport(context.active_object,1.0)
+        shape=shapeExport(context.active_object,1.0)
+        tree = ET.ElementTree(shape)
+        keywords = self.as_keywords()
+        f=open(self.filepath,"wb")
+        print (self.filepath)
+        tree.write(f)
+        #tree.close()
+        f.close()
         return {'FINISHED'}
 
 
