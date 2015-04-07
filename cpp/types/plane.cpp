f97ddf4ec4e4d351e68e6fb39d3115db15a8fac5
maciek <m@il.xx>
2015-04-07 19:03:27 +0200

    plane class

--- cpp/types/plane.cpp
+++ cpp/types/plane.cpp
@@ -1,1 +1,55 @@
 #include "types/plane.hpp"
+
+Plane::Plane(e_loc a, e_loc b, e_loc c, e_loc d) {
+    N=Vector3d(a,b,c);
+    D=d;
+}
+
+Plane::Plane(Vector3d normal, e_loc d) {
+    N=normal;
+    D=d;
+}
+
+Plane::Plane(const Plane &plane) {
+  N=plane.N;
+  D=plane.D;
+}
+
+Plane::Plane( Vector3d &vA, Vector3d &vB,  Vector3d &vC) {
+    Vector3d normalA,normalB;
+    normalA=(vC-vA)|1;
+    normalB=(vC-vA)|1;
+    N=(normalA^normalB)|1;
+    D=-vA % N;
+    //normalA=vC-vA;//|1;
+}
+
+const Plane & Plane::operator=(const Plane &plane) {
+    N=plane.N;
+    D=plane.D;
+    return *this;
+}
+
+const bool Plane::operator==(Plane &plane) {
+   return N == plane.N && D == plane.D;
+}
+
+const bool Plane::operator!=( Plane &plane) {
+    
+}
+
+const bool Plane::PointOnPlane( Vector3d & point) {
+   return DistanceToPlane(point)==0; 
+}
+
+const e_loc Plane::DistanceToPlane( Vector3d &point) {
+    return N % point +D;
+}
+
+const Vector3d Plane::RayIntersection( Vector3d &ray_pos,  Vector3d &ray_dir) {
+    e_loc a=N % ray_dir;
+    if (a==0) {
+        return ray_pos;
+    }
+    return ray_pos -  ray_dir * (DistanceToPlane(ray_pos)/a);
+}
--- hpp/types/plane.hpp
+++ hpp/types/plane.hpp
@@ -1,9 +1,23 @@
 #ifndef PLANE_HPP
 #define	PLANE_HPP
 
+#include "types/mathTypes.hpp"
+#include "types/types.hpp"
+
 class Plane {
 public:
-    
+    Vector3d N;
+    e_loc D;
+    Plane(e_loc a=1,e_loc b=0,e_loc c=0,e_loc d=0);
+    Plane(Vector3d normal,e_loc d=0);
+    Plane(const Plane &plane);
+    Plane( Vector3d &a,  Vector3d &b, Vector3d &c);
+    const Plane & operator=(const Plane &plane);
+    const bool operator==( Plane &plane);
+    const bool operator!=( Plane &plane);
+    const bool PointOnPlane( Vector3d & point);
+    const e_loc DistanceToPlane( Vector3d &point);
+    const Vector3d RayIntersection( Vector3d &ray_pos, Vector3d &ray_dir);
 };
 
 #endif	/* PLANE_HPP */
