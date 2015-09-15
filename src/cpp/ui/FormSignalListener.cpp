3e0dc3788d79bec788e7d7931c77214ba02dd14b
maciek <maciej.zakrzewski87@gmail.com>
2015-09-15 21:21:01 +0200

    dict data

--- src/cpp/ui/FormSignalListener.cpp
+++ src/cpp/ui/FormSignalListener.cpp
@@ -1,15 +1,28 @@
 #include <Rocket/Core/Event.h>
+#include <Rocket/Core/Dictionary.h>
+#include <Rocket/Core/Variant.h>
+
+#include <Rocket/Core/Header.h>
 
 #include "ui/FormSignalListener.hpp"
 
- FormSignalListener::FormSignalListener(string signal_name) : SignalListener(signal_name) {
-     
- }
+FormSignalListener::FormSignalListener(string signal_name) : SignalListener(signal_name) {
 
- 
- void FormSignalListener::ProcessEvent(Rocket::Core::Event& event) {
-     // {{0}} => params (json?)
-     
-     event.GetParameters();
-     PyScripting::getInstance()->broadcast(signal_name, {0}, true);
- }
+}
+
+void FormSignalListener::ProcessEvent(Rocket::Core::Event& event) {
+    // {{0}} => params (json?)
+
+    const Rocket::Core::Dictionary *d = event.GetParameters();
+    cout << "Dict" << endl;
+    //bool Iterate(int &pos, String& key, Variant* &value) const;
+    Rocket::Core::String s;
+    Rocket::Core::Variant *v;
+    int n;
+    while (d->Iterate(n, s, v)) {
+        Rocket::Core::String val = v->Get<Rocket::Core::String>();
+        cout << s.CString() << "=" << val.CString() << "---" << endl;
+    }
+
+    PyScripting::getInstance()->broadcast(signal_name,{0}, true);
+}
