03e5a19b08b194b14bbe85bcb116034e4834bc69
Maciej Zakrzewski <maciej.zakrzewski87@gmail.com>
2015-09-15 16:15:31 +0200

    sync

--- src/cpp/ui/FormSignalListener.cpp
+++ src/cpp/ui/FormSignalListener.cpp
@@ -1,3 +1,5 @@
+#include <Rocket/Core/Event.h>
+
 #include "ui/FormSignalListener.hpp"
 
  FormSignalListener::FormSignalListener(string signal_name) : SignalListener(signal_name) {
@@ -7,5 +9,7 @@
  
  void FormSignalListener::ProcessEvent(Rocket::Core::Event& event) {
      // {{0}} => params (json?)
+     
+     event.GetParameters();
      PyScripting::getInstance()->broadcast(signal_name, {0}, true);
  }
