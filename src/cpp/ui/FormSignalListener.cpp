4b4c2a4e7a58977250fa923c8bd648a410be360c
maciek <maciej.zakrzewski87@gmail.com>
2015-09-16 20:45:47 +0200

    fixes

--- src/cpp/ui/FormSignalListener.cpp
+++ src/cpp/ui/FormSignalListener.cpp
@@ -11,19 +11,14 @@ FormSignalListener::FormSignalListener(string signal_name) : SignalListener(sign
 }
 
 void FormSignalListener::ProcessEvent(Rocket::Core::Event& event) {
-    // {{0}} => params (json?)
-
     const Rocket::Core::Dictionary *d = event.GetParameters();
     UISignalData *data = new UISignalData();
-    cout << "Dict" << endl;
-    //bool Iterate(int &pos, String& key, Variant* &value) const;
     Rocket::Core::String s;
     Rocket::Core::Variant *v;
     int n;
     while (d->Iterate(n, s, v)) {
         Rocket::Core::String val = v->Get<Rocket::Core::String>();
-        data->set(string(s.CString()), string(val.CString()));
-        cout << s.CString() << "=" << val.CString() << "---" << endl;
+        data->set(string(s.CString()), string(val.CString()));        
     }
 
     PyScripting::getInstance()->broadcast(signal_name,{data}, true, true);
--- src/hpp/ui/UISignalData.hpp
+++ src/hpp/ui/UISignalData.hpp
@@ -3,11 +3,13 @@
 
 #include <string>
 #include <map>
+#include <Rocket/Core.h>
 
 using namespace std;
 
 class UISignalData {
     map<string, string> sigmap;
+    Rocket::Core::ElementDocument *element;
 public:
     void set(string k, string v);
     string get(string k);
