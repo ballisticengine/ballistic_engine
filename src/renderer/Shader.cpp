ff917c5d8c7e4fbe67a80db68697531a561696e8
maciek <maciej.zakrzewski87@gmail.com>
2015-10-19 21:49:45 +0200

    shader stub

---
+++ src/renderer/Shader.cpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   Shader.cpp
+ * Author: maciek
+ * 
+ * Created on 19 października 2015, 21:49
+ */
+
+#include "Shader.hpp"
+
+Shader::Shader() {
+}
+
+Shader::~Shader() {
+}
+
---
+++ src/renderer/Shader.hpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   Shader.hpp
+ * Author: maciek
+ *
+ * Created on 19 października 2015, 21:49
+ */
+
+#ifndef SHADER_HPP
+#define	SHADER_HPP
+
+class Shader {
+protected:
+    
+public:
+    Shader();
+    virtual ~Shader();
+};
+
+#endif	/* SHADER_HPP */
+
